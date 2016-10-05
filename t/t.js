cccc238748394e643db27c93f9084dea2fbe31e1
Qazzian <Qazzian@gmail.com>
2016-10-05 18:09:24 +0100

    Got QUnit working with steal.js

--- t/t.js
+++ t/t.js
@@ -1,11 +1,37 @@
+// import Backbone from "backbone";
 
-import $ from "jquery";
-import _ from "underscore";
 
-window._ = _;
+// import $ from "jquery";
+// import _ from "underscore";
 
-import Backbone from "backbone";
-import qunit from "qunitjs";
-import qunit_extended from "t/qunit_extended";
+// window._ = _;
+console.log('running t.js');
 
-steal.import('t/t_BlockManager');
+import QUnit from 'steal-qunit';
+
+// steal('steal-qunit', function(QUnit){
+// "use strict";
+
+function runTest(QUnit) {
+    "use strict";
+
+    console.log('loaded qUnit', QUnit);
+
+    QUnit.module('Qunit sanity checks');
+    console.log('module setup', typeof QUnit.module, typeof QUnit.test);
+    debugger;
+    QUnit.test('QUnit is defined', function (assert) {
+        assert.expect(1);
+        assert.ok(QUnit, 'QUnit is defined');
+        console.info('ran test');
+    });
+
+    console.log('defined test');
+};
+
+runTest(QUnit);
+
+// });
+
+// System.import("qunitjs").then(runTest);
+// steal.import('t/t_BlockManager');
