fa5113e7d75471a9c8547f5a808629b6aaf6d93b
Qazzian <Qazzian@gmail.com>
2016-10-05 18:14:23 +0100

    Clean up t.js

--- t/t.js
+++ t/t.js
@@ -1,37 +1,15 @@
-// import Backbone from "backbone";
-
-
-// import $ from "jquery";
-// import _ from "underscore";
-
-// window._ = _;
-console.log('running t.js');
-
 import QUnit from 'steal-qunit';
 
-// steal('steal-qunit', function(QUnit){
-// "use strict";
-
-function runTest(QUnit) {
+function runSanityCheck(QUnit) {
     "use strict";
 
-    console.log('loaded qUnit', QUnit);
-
     QUnit.module('Qunit sanity checks');
-    console.log('module setup', typeof QUnit.module, typeof QUnit.test);
-    debugger;
     QUnit.test('QUnit is defined', function (assert) {
         assert.expect(1);
         assert.ok(QUnit, 'QUnit is defined');
-        console.info('ran test');
     });
-
-    console.log('defined test');
 };
 
-runTest(QUnit);
+runSanityCheck(QUnit);
 
-// });
-
-// System.import("qunitjs").then(runTest);
 // steal.import('t/t_BlockManager');
